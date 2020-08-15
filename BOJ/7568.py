b37a7b8354725880283f151c3940b409478eeff5
Park Hoijai <hoijae0194@naver.com>
2020-08-15 18:37:06 +0900

    Python Algorithm

---
+++ BOJ/7568.py
@@ -0,0 +1,26 @@
+# 덩치
+
+T = int(input())
+
+people = []
+
+for _ in range(1,T+1):
+    w, h = map(int, input().split())
+    people.append((w, h))
+
+for i in people:
+    rank = 1
+    for j in people:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+    print(rank, end=' ')
+
+
+'''
+5
+55 185
+58 183
+88 186
+60 175
+46 155
+'''
