dd6741de5835839d540788fd520147a3aa992cfe
Park Hoijai <hoijae0194@naver.com>
2020-08-23 16:20:45 +0900

    Python Algorithm

--- theory/DBN/5-10 응용.py
+++ theory/DBN/5-10 응용.py
@@ -4,16 +4,16 @@ dy = [0, 0, 1, -1]
 dx = [-1, 1, 0, 0]
 
 
-def dfs(y, x):
-    visited[y][x] = True
+def dfs(x, y):
+    visited[x][y] = True
 
     for i in range(4):
-        ny = y + dy[i]
         nx = x + dx[i]
+        ny = y + dy[i]
 
-        if 0 <= ny < n and 0 <= nx < m:
-            if not visited[ny][nx] and graph[ny][nx] == 0:
-                dfs(ny, nx)
+        if 0 <= nx < n and 0 <= ny < m:
+            if not visited[nx][ny] and graph[nx][ny] == 0:
+                dfs(nx, ny)
 
 
 n, m = map(int, input().split())
@@ -24,11 +24,11 @@ ans = 0
 
 for _ in range(n):
     graph.append(list(map(int, input())))
-
+print(graph)
 for a in range(n):
     for b in range(m):
         if graph[a][b] == 0 and not visited[a][b]:
             ans += 1
             dfs(a, b)
 
-print(ans)
+# print(ans)
--- theory/DBN/5-10.py
+++ theory/DBN/5-10.py
@@ -1,18 +1,13 @@
-# 음료수 얼려 먹기
-n, m = map(int, input().split()) # 세로가 X축, 가로가 Y축
-
-graph = [] # 초기화
-
-for _ in range(n):
-    graph.append(list(map(int, input())))
-
-
 def dfs(x, y):
+    # 매트릭스의 범위를 벗어나면 즉시 종료
     if x <= -1 or x >= n or y <= -1 or y >= m:
         return False
-    if graph[x][y] == 0:
-        graph[x][y] = 1
 
+    # 현재의 위치를 방문하지 않았다면
+    if graph[x][y] == 0:
+        graph[x][y] = 1 # 방문 처리를 해준다
+
+        # 상, 하, 좌, 우의 위치를 모두 재귀적으로 호출
         dfs(x - 1, y)
         dfs(x, y - 1)
         dfs(x + 1, y)
@@ -21,12 +16,16 @@ def dfs(x, y):
     return False
 
 
-ans = 0
+n, m = map(int, input().split())
 
-for i in range(n): # X 축
-    for j in range(m): # Y 축
-        if dfs(i, j) == True:
-            ans += 1
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input())))
 
+cnt = 0
+for a in range(n): # (0,0) 부터 (n, m) 까지 전체 탐색 시작
+    for b in range(m):
+        if dfs(a, b): # 현재의 위치에서 dfs를 실행해서 True가 되면,
+            cnt += 1 # 카운트 + 1
+print(cnt)
 
-print(ans)
