2018efbf14019590d80b3a359d094749cc014745
Park Hoijai <hoijae0194@naver.com>
2020-09-05 10:55:38 +0900

    Python Algorithm

---
+++ BOJ/1003.py
@@ -0,0 +1,22 @@
+# 피보나치 함수
+
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+def fibonacci_cnt(n):
+    zero_cnt = [1, 0]
+    one_cnt = [0, 1]
+    if n <= 1:
+        return
+
+    for i in range(2, n + 1):
+        zero_cnt.append(zero_cnt[i - 1] + zero_cnt[i - 2])
+        one_cnt.append(one_cnt[i - 1] + one_cnt[i - 2])
+
+    return zero_cnt, one_cnt
+
+zero_count, one_count = fibonacci_cnt(40)
+
+for _ in range(int(input())):
+    m = int(input())
+    print(f'{zero_count[m]} {one_count[m]}')
---
+++ BOJ/2748.py
@@ -0,0 +1,17 @@
+# 피보나치 함수2
+
+import sys
+n = int(sys.stdin.readline().rstrip())
+
+d = [0] * 100
+
+def fibo(x):
+    if x == 1 or x == 2:
+        return 1
+
+    if d[x] != 0:
+        return d[x]
+    d[x] = fibo(x-1) + fibo(x-2)
+    return d[x]
+
+print(fibo(n))
