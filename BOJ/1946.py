d9b6277535a2727d03b1dcd4ebed8ac7c6e49dca
Park Hoijai <hoijae0194@naver.com>
2020-09-10 10:10:49 +0900

    Python Algorithm

---
+++ BOJ/1946.py
@@ -0,0 +1,21 @@
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+for _ in range(int(input())):
+    arr = []
+    n = int(input())
+
+    for _ in range(n):
+        arr.append(list(map(int, input().split())))
+    arr.sort(key = lambda x : x[0])
+
+    tmp = arr[0][1]
+    ans = 0
+
+    for i in range(1, n):
+        if arr[i][1] > tmp:
+            ans += 1
+        else:
+            tmp = arr[i][1]
+
+    print(n - ans)
