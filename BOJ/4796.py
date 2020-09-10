c1255ad00848982e00b2c93c0cd2deeb9e78a408
Park Hoijai <hoijae0194@naver.com>
2020-09-11 01:37:11 +0900

    Python Algorithm

---
+++ BOJ/4796.py
@@ -0,0 +1,13 @@
+# 캠핑
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+cnt = 0
+while True:
+    cnt += 1
+    L, P, V = map(int, input().split())
+
+    if L == 0 and P == 0 and V == 0: break
+
+    vacation = (V//P) * L + min(L, V- (V//P) * P)
+    print(f'Case {cnt}: {vacation}')
