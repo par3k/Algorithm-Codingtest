805c1024b7b34ff55048e51f4fa914226b5f2c94
Park Hoijai <hoijae0194@naver.com>
2020-09-14 00:59:38 +0900

    Python Algorithm

--- BOJ/11497.py
+++ BOJ/11497.py
@@ -6,7 +6,7 @@ for _ in range(int(input())):
     n = int(input())
     arr = list(map(int, input().split()))
     arr.sort()
-    print(arr)
+    # print(arr)
     ans, front, rear = 0, 0, n-1
     woods_arr = [0] * n
 
@@ -17,7 +17,7 @@ for _ in range(int(input())):
         else:
             woods_arr[front] = arr[i]
             front += 1
-    print(woods_arr)
+    # print(woods_arr)
     for i in range(1, n):
         ans = max(ans, abs(woods_arr[i] - woods_arr[i-1]))
 
