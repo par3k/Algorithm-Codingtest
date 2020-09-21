6213dac2859bc9b04b0c1ca2839cf00daa018994
Park Hoijai <hoijae0194@naver.com>
2020-09-21 18:24:06 +0900

    Python Algorithm

---
+++ BOJ/11724-unionfind.py
@@ -0,0 +1,36 @@
+# 연결 요소의 갯수
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+
+def union(a, b):
+    a = find(parent, a)
+    b = find(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+v, e = map(int, input().split())
+
+parent = [0] * (v+1)
+for i in range(1, v+1):
+    parent[i] = i
+
+arr = [list(map(int, input().split())) for _ in range(e)]
+arr.sort(key=lambda x:min(x))
+
+for i in arr:
+    a, b = i
+    if find(parent, a) != find(parent, b):
+        union(a, b)
+
+parent.pop(0)
+print(len(set(parent)))
---
+++ theory/DBN/10-4.py
@@ -0,0 +1,38 @@
+# 서로소 집합을 활용한 사이클 판별 소스코드
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+v, e = map(int, input().split())
+parent = [0] * (v+1)
+
+for i in range(1, v+1):
+    parent[i] = i
+
+cycle = False
+
+for i in range(e):
+    a, b = map(int, input().split())
+    if find_parent(parent, a) == find_parent(parent, b):
+        cycle = True
+        break
+    else:
+        union_parent(parent, a, b)
+
+if cycle:
+    print('사이클이 발생함')
+else:
+    print('사이클이 발생하지 않음')
