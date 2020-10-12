b8ca08e10b13565773d4b4ca1a70f755338e69d9
Park Hoijai <hoijae0194@naver.com>
2020-10-12 17:55:02 +0900

    Python Algorithm

--- BOJ/4963.py
+++ BOJ/4963.py
@@ -1,17 +1,36 @@
 # 섬의 갯수
-'''
-1 0 1 0 0
-1 0 0 0 0
-1 0 1 0 1
-1 0 0 1 0
-'''
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+sys.setrecursionlimit(100001)
+
+dx, dy = [-1, -1, 0, 1, 1, 1, 0, -1], [0, 1, 1 ,1, 0, -1, -1, -1]
+
+
+def dfs(x, y):
+    visited[x][y] = True
+
+    for i in range(8):
+        nx, ny = x + dx[i], y + dy[i]
+
+        if nx >= h or nx < 0 or ny >= w or ny < 0:
+            continue
+
+        if not visited[nx][ny] and graph[nx][ny] == 1:
+            dfs(nx, ny)
+
 
 while True:
     w, h = map(int, input().split())
-    visited = [0] * (w * h)
-    if w == 0 and h == 0:
-        break
+    if w == 0 and h == 0: break
+    graph = [list(map(int, input().split())) for _ in range(h)]
 
+    visited = [[False] * w for _ in range(h)]
+    ans = 0
 
-    print(w, h)
-    print(visited)
+    for i in range(h):
+        for j in range(w):
+            if not visited[i][j] and graph[i][j] == 1:
+                dfs(i, j)
+                ans += 1
+
+    print(ans)
