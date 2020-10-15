0d071c6fa6080978f56f95b0f4829b5c1a31aade
Park Hoijai <hoijae0194@naver.com>
2020-10-16 00:24:32 +0900

    Python Algorithm

---
+++ BOJ/1929.py
@@ -0,0 +1,13 @@
+# 소수 구하기
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+m, n = map(int, input().split())
+
+arr = [0] * (n + 1)
+
+for i in range(2, n+1):
+    if arr[i] == 0:
+        if i >= m: print(i)
+        for j in range(i, n+1, i):
+            arr[j] = 1
