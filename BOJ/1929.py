3c2264e1bfc2cd68d588a6c0923e0b886d53a70d
Park Hoijai <hoijae0194@naver.com>
2020-10-16 11:12:54 +0900

    Python Algorithm

--- BOJ/1929.py
+++ BOJ/1929.py
@@ -9,5 +9,5 @@ arr = [0] * (n + 1)
 for i in range(2, n+1):
     if arr[i] == 0:
         if i >= m: print(i)
-        for j in range(i, n+1, i):
+        for j in range(i, n+1, i): # i의 배수로 순환함
             arr[j] = 1
