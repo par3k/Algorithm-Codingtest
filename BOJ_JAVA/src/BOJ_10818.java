efa982e3ef827f8017ed37fcdaed552366663e8a
Park Hoijai <hoijae0194@naver.com>
2020-11-10 10:05:43 +0900

    Python Algorithm

---
+++ BOJ_JAVA/src/BOJ_10818.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_10818 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        String[] mm = br.readLine().split(" ");
+
+        int min = 1000001;
+        int max = -1000001;
+
+        for (int i = 0; i < n; i++) {
+            if (min > Integer.parseInt(mm[i])){
+                min = Integer.parseInt(mm[i]);
+            }
+            if (max < Integer.parseInt(mm[i])){
+                max = Integer.parseInt(mm[i]);
+            }
+        }
+        bw.write(Integer.toString(min) + " " + Integer.toString(max));
+        bw.close();
+        br.close();
+
+    }
+}
---
+++ BOJ_JAVA/src/BOJ_2562.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class BOJ_2562 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+        int place = 1;
+
+        while (i < 9) {
+            int new_n = sc.nextInt();
+            i ++;
+            if (new_n > n) {
+                n = new_n;
+                place = i;
+            }
+        }
+        System.out.println(n);
+        System.out.println(place);
+    }
+}
