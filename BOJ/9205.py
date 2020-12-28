53c95cf4131001fb582974cf003b4235834c4891
Park Hoijai <hoijae0194@naver.com>
2020-12-28 09:41:34 +0900

    Python Algorithm

--- BOJ/9205.py
+++ BOJ/9205.py
@@ -1,30 +1,43 @@
 # 맥주 마시면서 걸어가기
 import sys
+from collections import deque
 input = lambda : sys.stdin.readline().rstrip()
-sys.setrecursionlimit(10 ** 8)
-
-def make():
-    for i in range(n + 2):
-        for j in range(n + 2):
-            if i == j: continue
-            if abs(graph[i][0] - graph[j][0]) + abs(graph[i][1] - graph[j][1]) <= 1000:
-                graph_[i][j], graph_[j][i] = 1, 1
 
 
-def dfs(start):
+def check(point1, point2):
+    dist = abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
+    if dist <= 1000:
+        return True
+    else:
+        return False
+
+
+def bfs(start):
+    queue = deque()
+    queue.append(start)
     visited[start] = True
-    for i in range(n + 2):
-        if graph_[start][i] == 1 and not visited[i]:
-            dfs(i)
+
+    while queue:
+        pos = queue.popleft()
+        for i in edges[pos]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
 
 
 for _ in range(int(input())):
     n = int(input())
     graph = [list(map(int, input().split())) for _ in range(n + 2)]
-    graph_ = [[0] * (n + 2) for _ in range(n + 2)]
+    edges = [[] for _ in range(n + 2)]
     visited = [False for _ in range(n + 2)]
-    make()
-    dfs(0)
+
+    for i in range(n + 2):
+        for j in range(n + 2):
+            if i != j:
+                if check(graph[i], graph[j]):
+                    edges[i].append(j)
+    bfs(0)
+
     if visited[n + 1]:
         print("happy")
     else:
