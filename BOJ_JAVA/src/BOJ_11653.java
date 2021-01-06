42967f5fdcbc69fb29c43d6aa8c61dcd4bf6bccf
Park Hoijai <hoijae0194@naver.com>
2021-01-06 23:17:12 +0900

    Java Algorithm

---
+++ BOJ_JAVA/src/BOJ_11653.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class BOJ_11653 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int i = 2;
+        while ( n != 1) {
+            if (n % i == 0) {
+                n /= i;
+                System.out.println(i);
+            } else {
+                i ++;
+            }
+        }
+        br.close();
+    }
+}
