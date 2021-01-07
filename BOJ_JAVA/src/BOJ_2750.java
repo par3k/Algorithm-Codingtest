f8243b808fc02936b484b444d78eaff2b2d5c481
Park Hoijai <hoijae0194@naver.com>
2021-01-07 23:05:50 +0900

    Java Algorithm

---
+++ BOJ_JAVA/src/BOJ_2750.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class BOJ_2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int tmp;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (arr[i] > arr[j]) {
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        br.close();
+    }
+}
