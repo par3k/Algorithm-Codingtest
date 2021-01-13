91a19936b0adb927a0c4bacc6278f9e876733acc
Park Hoijai <hoijae0194@naver.com>
2021-01-14 07:58:03 +0900

    Java Algorithm

---
+++ BOJ_JAVA/src/BOJ_14650.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class BOJ_14650 {
+    static int ans;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        ans = 0;
+
+        dfs(0, 0, N);
+        System.out.println(ans);
+        br.close();
+    }
+
+    public static void dfs(int n, int sum, int finish) {
+        for (int i = 0; i < 3; i++) {
+            if (n == 0 && i == 0) continue;
+
+            if (n == finish) {
+                if (sum % 3 == 0) {
+                    ans++;
+                    return;
+                }
+            } else {
+                dfs(n + 1, sum + i, finish);
+            }
+        }
+    }
+}
