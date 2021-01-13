354b619cce2768d9a9a4bf37df2425d8de6ecb7c
Park Hoijai <hoijae0194@naver.com>
2021-01-14 08:37:58 +0900

    Python Algorithm

---
+++ BOJ/2608.py
@@ -0,0 +1,88 @@
+# 로마 숫자
+
+num = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+extra = {'IV' : 4, 'IX' : 9, 'XL' : 40, 'XC' : 90, 'CD' : 400, 'CM' : 900}
+
+num1, num2 = input(), input()
+
+
+def toNum(r):
+    res = 0
+    n = len(r)
+    idx = 0
+
+    while idx < n:
+        if idx == n - 1:
+            res += num[r[idx]]
+            break
+
+        flag = True
+        if r[idx] == 'I':
+            if r[idx + 1] == 'V' or r[idx + 1] == 'X':
+                res += extra[r[idx : idx + 2]]
+                flag = False
+        elif r[idx] == 'X':
+            if r[idx + 1] == 'L' or r[idx + 1] == 'C':
+                res += extra[r[idx : idx + 2]]
+                flag = False
+
+        elif r[idx] == 'C':
+            if r[idx + 1] == 'D' or r[idx + 1] == 'M':
+                res += extra[r[idx : idx + 2]]
+                flag = False
+
+        if not flag:
+            idx += 2
+        else:
+            res += num[r[idx]]
+            idx += 1
+
+    return res
+
+
+a, b = toNum(num1), toNum(num2)
+total = a + b
+print(total)
+
+ans = ""
+total = str(total)
+t = len(total)
+n = len(total)
+
+while n:
+    nums = int(total[t - n])
+    if n == 4:
+        ans += 'M' * nums
+    elif n == 3:
+        if num == 9:
+            ans += 'CM'
+        elif num == 4:
+            ans += 'CD'
+        else:
+            if num >= 5:
+                ans += 'D'
+            ans += 'C' * (nums % 5)
+
+    elif n == 2:
+        if num == 9:
+            ans += 'XC'
+        elif num == 4:
+            ans += 'XL'
+        else:
+            if num >= 5:
+                ans += 'L'
+            ans += 'X' * (num % 5)
+
+    elif n == 1:
+        if num == 9:
+            ans += 'IX'
+        elif num == 4:
+            ans += 'IV'
+        else:
+            if num >= 5:
+                ans += 'V'
+            ans += 'I' * (num % 5)
+    n -= 1
+print(ans)
+
+
