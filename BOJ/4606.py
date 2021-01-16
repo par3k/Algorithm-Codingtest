94f247b1c031ba75e5e6e43bec93b72ac8ca02fe
Park Hoijai <hoijae0194@naver.com>
2021-01-16 12:05:36 +0900

    Python Algorithm

---
+++ BOJ/4606.py
@@ -0,0 +1,28 @@
+# The Seven Percent Solution
+import sys
+
+while True:
+    a = list(map(str, sys.stdin.readline().rstrip()))
+    
+    if a[0] == "#":
+        break
+
+    for i in range(len(a)):
+        if a[i] == " ":
+            a[i] = "%20"
+        elif a[i] == "!":
+            a[i] = "%21"
+        elif a[i] == "$":
+            a[i] = "%24"
+        elif a[i] == "%":
+            a[i] = "%25"
+        elif a[i] == "(":
+            a[i] = "%28"
+        elif a[i] == ")":
+            a[i] = "%29"
+        elif a[i] == "*":
+            a[i] = "%2a"
+        else:
+            continue
+
+    print(''.join(a))
