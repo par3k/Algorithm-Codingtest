af1b4957190ea169980317411a9fb7705d9c4c99
Park Hoijai <hoijae0194@naver.com>
2021-01-17 17:17:55 +0900

    Python Algorithm

--- BOJ/13275.py
+++ BOJ/13275.py
@@ -1,40 +1,26 @@
 # 가장 긴 팰린드롬 부분 문자열 - 못 품
+import sys
+input = lambda : sys.stdin.readline().rstrip()
 
-s = list(input())
-n = len(s)
 
-s += [0 for _ in range(200000)]
-r = [0 for _ in range(200000)]
+def longesPalindrome(s):
+    t = '#'.join('({})'.format(s))
+    n = len(t)
+    p = [0] * n
+    c = r = 0
 
-for i in range(2 * n - 2, -1, -1):
-    if i % 2 == 0:
-        s[i] = s[i / 2]
-    else:
-        s[i] = '#'
+    for i in range(1, n- 1):
+        p[i] = (r >i) and min(r - i, p[2 * c - i])
 
-ans = 0
-p = rmax = -1
-n = 2 * n - 1
+        while t[i + 1 + p[i]] == t[i - 1 - p[i]]:
+            p[i] += 1
 
-for i in range(n):
-    if i <= rmax:
-        r[i] = min(rmax - i, r[2 * p - i])
-    j = r[i] + 1
-    while i - j > 0 and i + j < n:
-        if s[i - j] == s[i + j]:
-            r[i] += 1
-        else:
-            break
-        j += 1
-    if rmax < i + r[i]:
-        p = i
-        rmax = p + r[p]
+        if i + p[i] > r:
+            c, r = i, i + p[i]
 
-for i in range(n):
-    if i % 2 == 0:
-        if r[i] / 2 * 2 + 1 > ans:
-            ans = r[i] / 2 * 2 + 1
-        else:
-            if (r[i] + 1) / 2 * 2 > ans:
-                ans = (r[i] + 1) / 2 * 2
-print(ans)
+    return max(p)
+
+
+s = input()
+
+print(longesPalindrome(s))
---
+++ BOJ/20124.py
@@ -0,0 +1,19 @@
+# 모르고리즘 회장님 추천 받습니다.
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+max = 0
+ans = 'zzzzzzzzz'
+
+for i in range(int(input())):
+    name, score = input().split()
+    if int(score) > max:
+        max = int(score)
+        ans = name
+
+    elif int(score) == max:
+        if name < ans:
+            max = int(score)
+            ans = name
+
+print(ans)
