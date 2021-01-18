1e2092a6879f04857fd4772ef7f8707ed19e57ee
Park Hoijai <hoijae0194@naver.com>
2021-01-18 22:30:03 +0900

    Java Algorithm

--- BOJ_JAVA/src/BOJ_10872.java
+++ BOJ_JAVA/src/BOJ_10872.java
@@ -1,17 +1,20 @@
 import java.io.*;
 
 public class BOJ_10872 {
-    public static int recursive (int i) {
-        if (i <= 1) {
+    
+    public static int pactorial(int n) {
+        if (n <= 1) {
             return 1;
         } else {
-            return i * recursive(i - 1);
+            return pactorial(n - 1) * n;
         }
     }
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        System.out.println(recursive(n));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(pactorial(N));
 
     }
 }
