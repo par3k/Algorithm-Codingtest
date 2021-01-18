758e03a51a07b51f9a1c1b5cf62d58f67e7a473f
Park Hoijai <hoijae0194@naver.com>
2021-01-18 22:50:39 +0900

    Java Algorithm

---
+++ BOJ_JAVA/src/BOJ_11047.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class BOJ_11047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        Integer[] arr = new Integer[N];
+        int ans = 0;
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for (int i = 0; i < arr.length; i++) {
+            ans += K / arr[i];
+            K = K % arr[i];
+        }
+        System.out.println(ans);
+        br.close();
+    }
+}
