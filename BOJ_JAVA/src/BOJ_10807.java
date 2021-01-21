61fa3c72c8b033a4a6eeeac012f1433eee13766c
Park Hoijai <hoijae0194@naver.com>
2021-01-21 22:49:18 +0900

    Java Algorithm

---
+++ BOJ_JAVA/src/BOJ_10807.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_10807 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int [] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int v = Integer.parseInt(br.readLine());
+
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == v) {
+                ans++;
+            }
+        }
+        System.out.println(ans);
+        br.close();
+    }
+}
