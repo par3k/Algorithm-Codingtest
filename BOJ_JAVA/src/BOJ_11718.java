9e30b7a050ce26964314040179913d8b68b354d6
Park Hoijai <hoijae0194@naver.com>
2021-01-21 09:13:54 +0900

    Java Algorithm

---
+++ BOJ_JAVA/src/BOJ_11718.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class BOJ_11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> lst = new ArrayList<String>();
+        String line;
+        while ((line = br.readLine()) != null) {
+            lst.add(line);
+        }
+        for (int i = 0; i < lst.size(); i++) {
+            if ( i == lst.size()) {
+                System.out.println(lst.get(i));
+            }
+            else {
+                System.out.println(lst.get(i));
+            }
+        }
+    }
+}
