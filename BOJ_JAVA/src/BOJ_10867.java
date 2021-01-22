df721d4454f223f2e02aadbd316d9800b253a885
Park Hoijai <hoijae0194@naver.com>
2021-01-23 01:20:45 +0900

    Java Algorithm

--- BOJ_JAVA/src/BOJ_10867.java
+++ BOJ_JAVA/src/BOJ_10867.java
@@ -6,9 +6,10 @@ import java.util.StringTokenizer;
 public class BOJ_10867 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         int[] arr = new int[2001];
+
         for (int i = 0; i < N; i++) {
             arr[Integer.parseInt(st.nextToken()) + 1000]++;
         }
