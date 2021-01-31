f2426af9f994bb9cc8807ec1ac0b7ff865cd805f
Park Hoijai <hoijae0194@naver.com>
2021-01-31 19:38:22 +0900

    Java Algorithm

---
+++ BOJ_JAVA/src/BOJ/BOJ_15184.java
@@ -0,0 +1,26 @@
+package BOJ;
+
+import java.io.*;
+import java.util.Locale;
+
+public class BOJ_15184 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine().toUpperCase(Locale.ROOT);
+
+        int[] cnt = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') cnt[s.charAt(i) - 'A']++;
+        }
+        
+        for (int i = 0; i < 26; i++) {
+            System.out.print((char) (i + 65) + " | ");
+            while (cnt[i] != 0) {
+                System.out.print('*');
+                cnt[i]--;
+            }
+            System.out.println();
+        }
+    }
+}
