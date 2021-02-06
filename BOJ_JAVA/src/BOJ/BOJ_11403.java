defe077394f74f4bb81c875591cdebc0b8d022f0
Park Hoijai <hoijae0194@naver.com>
2021-02-06 21:34:53 +0900

    Java Algorithm

--- BOJ_JAVA/src/BOJ/BOJ_11403.java
+++ BOJ_JAVA/src/BOJ/BOJ_11403.java
@@ -1,29 +1,29 @@
 package BOJ;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class BOJ_11403 {
+    private static int N;
     private static int[][] graph;
     private static boolean[] visited;
-    private static int N;
 
     private static void dfs(int x, int y) {
         visited[y] = true;
         graph[x][y] = 1;
 
-        for (int i =0 ; i < N; i++) {
-            if (graph[y][i] == 1 && visited[i] == false) {
+        for (int i = 0; i < N; i++) {
+            if (graph[y][i] == 1 && !visited[i]) {
                 dfs(x, i);
             }
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
+
         graph = new int[N][N];
         visited = new boolean[N];
 
@@ -38,18 +38,22 @@ public class BOJ_11403 {
             for (int j = 0; j < N; j++) {
                 visited[j] = false;
             }
+
             for (int j = 0; j < N; j++) {
-                if (graph[i][j] == 1 && visited[j] == false) {
+                if (graph[i][j] == 1 && !visited[j]) {
                     dfs(i, j);
                 }
             }
         }
 
         for (int i = 0; i < N; i++) {
-            for (int j= 0; j <N; j++) {
-                System.out.print(graph[i][j] + " ");
+            for (int j = 0; j < N; j++) {
+                bw.write(graph[i][j] + " ");
             }
-            System.out.println();
+            bw.write("\n");
         }
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
