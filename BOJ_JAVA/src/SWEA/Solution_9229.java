7d05b92757425431f3c7b0b8c1e9bcbf3676523a
Park Hoijai <hoijae0194@naver.com>
2021-02-08 20:37:53 +0900

    Java Algorithm

---
+++ BOJ_JAVA/src/SWEA/Solution_9229.java
@@ -0,0 +1,40 @@
+package SWEA;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Solution_9229 {
+    private static int N, M;
+    private static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc < T+1; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            arr =  new int[N];
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int ans = -1;
+            for (int i = 0; i < N; i++) {
+                for (int j = i + 1; j < N; j++) {
+                    if (arr[i] + arr[j] <= M) {
+                        ans = Math.max(ans, arr[i] + arr[j]);
+                    }
+                }
+            }
+            bw.write("#" + tc + " " + ans + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
