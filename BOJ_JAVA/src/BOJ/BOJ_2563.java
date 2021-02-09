e227f1c1833c6bd01e84d64be5e7ad104558f016
Park Hoijai <hoijae0194@naver.com>
2021-02-09 20:42:49 +0900

    Java Algorithm

---
+++ BOJ_JAVA/src/BOJ/BOJ_2563.java
@@ -0,0 +1,32 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2563 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] graph = new int[100][100];
+        int ans = 0;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    if (graph[j][k] == 1) continue;
+                    graph[j][k] = 1;
+                    ans++;
+                }
+            }
+        }
+        System.out.println(ans);
+        br.close();
+    }
+}
+
