4f198ef38a03883108d7cb6a25cfa4bc4dd14856
Park Hoijai <hoijae0194@naver.com>
2021-02-17 22:39:31 +0900

    Java Algorithm

---
+++ BOJ_JAVA/src/BOJ/BOJ_11399.java
@@ -0,0 +1,29 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_11399 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N - i; j++) {
+                ans += arr[j];
+            }
+        }
+        System.out.println(ans);
+    }
+}
