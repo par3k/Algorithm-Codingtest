51c0a74caa400807748fb6f59fed13b57f5acff7
Park Hoijai <hoijae0194@naver.com>
2021-02-23 01:36:24 +0900

    Java Algorithm

--- BOJ_JAVA/src/BOJ/BOJ_2567.java
+++ BOJ_JAVA/src/BOJ/BOJ_2567.java
@@ -7,33 +7,42 @@ import java.util.StringTokenizer;
 
 public class BOJ_2567 {
     private static int[][] graph;
+    private static int[] dx = {-1, 1, 0, 0};
+    private static int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        graph = new int[100][100];
+        graph = new int[101][101];
 
         int N = Integer.parseInt(br.readLine());
 
-        for (int tc = 1; tc <= N; tc++) {
+        for (int t = 0; t < N; t++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            for (int i = x ; i < x + 10; i++) {
-                for (int j = y; j < j + 10; j++) {
-                    if (graph[i][j] == 1) continue;
+            for (int i = x; i < x + 10; i++) {
+                for (int j = y; j < y + 10; j++) {
                     graph[i][j] = 1;
                 }
             }
+        }
+        int ans = 0;
+        for (int i = 0; i < graph.length; i++) {
+            for (int j = 0; j < graph[i].length; j++) {
+                if (graph[i][j] == 1) {
+                    for (int d = 0; d < 4; d++) {
+                        int nx = i + dx[d];
+                        int ny = j + dy[d];
 
-            int ans = 0;
-            for (int i = 0 ; i < 100; i++) {
-                for (int j = 0; j <100; j++) {
-                    if (graph[i][j] == 1) ans++;
+                        if (graph[nx][ny] == 0 || nx < 0 || nx > 100 || ny < 0 || ny > 100) {
+                            ans++;
+                        }
+                    }
                 }
             }
-            System.out.println(ans);
         }
-    br.close();
+        System.out.println(ans);
+        br.close();
     }
 }
