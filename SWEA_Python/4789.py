a8d731df4883a19dcd8dc7bf946c33b30d1ad24f
Park Hoijai <hoijae0194@naver.com>
2021-02-26 21:03:26 +0800

    Python Algorithm

---
+++ SWEA_Python/4789.py
@@ -0,0 +1,14 @@
+for tc in range(int(input())):
+    a = input()
+    people = 0
+    ans=  0
+
+    for need_pe, pe_cnt in enumerate(a):
+        pe_cnt = int(pe_cnt)
+        if need_pe <= people:
+            people += pe_cnt
+        else:
+            ans += need_pe - people
+            people += need_pe - people + pe_cnt
+
+    print(f'#{tc + 1} {ans}')
