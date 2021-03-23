f49dfa2ae86ba731c4f9842e310de08d7a6a26c3
Park Hoijai <hoijae0194@naver.com>
2021-03-23 20:50:00 +0900

    Python Algorithm

---
+++ theory_Python/DBN/다익스트라PQ.py
@@ -0,0 +1,37 @@
+import heapq, sys
+input = lambda :sys.stdin.readline().rstrip()
+INF = int(1e9)
+
+n, m = map(int, input().split())
+start = int(input())
+graph = [[] for _ in range(n + 1)]
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    from_, to_, weight = map(int, input().split())
+    graph[from_].append((to_, weight))
+
+
+def dijkstra(start):
+    queue = list()
+    heapq.heappush(queue, (0, start))
+    distance[start] = 0
+
+    while queue:
+        dist, now = heapq.heappop(queue)
+
+        if distance[now] < dist: continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(queue, (cost, i[0]))
+
+
+dijkstra(start)
+
+for i in range(1, n + 1):
+    if distance[i] == INF:
+        print("INF")
+    else:
+        print(distance[i])
