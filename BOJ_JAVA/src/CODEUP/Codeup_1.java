1524859a899286b5377e6c7098f4c8c8942c4dd7
Park Hoijai <hoijae0194@naver.com>
2021-03-25 09:23:35 +0900

    Java Algorithm

--- BOJ_JAVA/src/CODEUP/Codeup_1.java
+++
@@ -1,8 +0,0 @@
-package CODEUP;
-
-public class Codeup_1 {
-    public static void main(String[] args){
-
-        System.out.print("\"!@#$%^&*()\"");
-    }
-}
--- BOJ_JAVA/src/SSAFY/GoodMorning.java
+++ BOJ_JAVA/src/SSAFY/GoodMorning.java
@@ -1,3 +1,5 @@
+package SSAFY;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
