13480ba599bdbcd07439d218f9cd7af7f58975be
Park Hoijai <hoijae0194@naver.com>
2021-04-13 15:33:37 +0900

    Python Algorithm

---
+++ BOJ_Python/10282.py
@@ -0,0 +1,38 @@
+# 해킹
+import heapq, sys
+input = lambda :sys.stdin.readline().rstrip()
+INF = int(1e9)
+
+
+def dijikstra(start):
+    queue = list()
+    heapq.heappush(queue, (0, start))
+    distance[start] = 0
+
+    while queue:
+        cost, current = heapq.heappop(queue)
+        if distance[current] < cost: continue
+        for i in graph[current]:
+            cost2 = cost + i[1]
+            if cost2 < distance[i[0]]:
+                distance[i[0]] = cost2
+                heapq.heappush(queue, (cost2, i[0]))
+
+
+for _ in range(int(input())):
+    n, d, c = map(int, input().split())
+    graph = [[] for _ in range(n + 1)]
+    distance = [INF] * (n + 1)
+
+    for _ in range(d):
+        a, b, s = map(int, input().split())
+        graph[b].append([a, s])
+
+    dijikstra(c)
+    cnt = 0
+    ans = 0
+    for i in range(1, len(distance)):
+        if distance[i] != INF:
+            cnt += 1
+            ans = max(ans, distance[i])
+    print(cnt, ans)
