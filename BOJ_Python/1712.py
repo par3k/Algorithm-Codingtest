5afe58e44a6e8b5cc4aaba3913d6d8eb5fd31b1b
Park Hoijai <hoijae0194@naver.com>
2021-05-31 23:26:05 +0900

    Python Algorithm

--- BOJ_Python/1712.py
+++ BOJ_Python/1712.py
@@ -1,13 +1,5 @@
 # 손익분기점
-
 a, b, c = map(int,input().split())
-
-# n = 1
-# while 1:
-#     if a + ( b * n) < c * n:
-#         break
-#     n += 1
-# print(n)
 
 if b == c or b > c:
     print(-1)
