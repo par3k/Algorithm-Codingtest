709b016dffec690310e1a8d56247488b9c57e955
Park Hoijai <hoijae0194@naver.com>
2021-06-19 11:32:29 +0900

    Python Algorithm

---
+++ BOJ_Python/11279.py
@@ -0,0 +1,14 @@
+# 최대 힙
+import heapq, sys
+input = lambda :sys.stdin.readline().rstrip()
+
+arr = []
+for _ in range(int(input())):
+    a = int(input())
+    if a == 0:
+        if len(arr) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(arr)[1])
+    else:
+        heapq.heappush(arr, (-a, a))
