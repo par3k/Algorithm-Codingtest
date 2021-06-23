f1bb87a6ef0ff33908cf9914ef79afe3443777e1
Park Hoijai <hoijae0194@naver.com>
2021-06-23 23:15:17 +0900

    Python Algorithm

--- BOJ_Python/2630.py
+++ BOJ_Python/2630.py
@@ -1,34 +1,36 @@
 # 색종이 만들기
 import sys
 input = lambda :sys.stdin.readline().rstrip()
+sys.setrecursionlimit(1000001)
 
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
-blue, white = 0, 0
+w, b = 0, 0 # 답이 될 값들 지정
 
 
-def cut_graph(x, y, t):
-    global blue, white, graph
+def solution(x, y, t):
+    global w, b
     color = graph[x][y]
-    double_check = False
+    flag = 0
+
     for i in range(x, x + t):
-        if double_check:
-            break
+        if flag: break
         for j in range(y, y + t):
             if graph[i][j] != color:
-                cut_graph(x, y, t//2)
-                cut_graph(x + t//2, y, t//2)
-                cut_graph(x, y + t//2, t//2)
-                cut_graph(x + t//2, y + t//2, t//2)
-                double_check = True
+                solution(x, y, t//2)
+                solution(x + t//2, y, t//2)
+                solution(x, y + t//2, t//2)
+                solution(x + t//2, y + t//2, t//2)
+                flag = 1
                 break
-    if not double_check:
+
+    if not flag:
         if graph[x][y] == 1:
-            blue += 1
+            b += 1
         else:
-            white += 1
+            w += 1
 
 
-cut_graph(0, 0, n)
-print(white)
-print(blue)
+solution(0, 0, n)
+print(w)
+print(b)
