4d7ee4ff6f14d8c2d5acf245694e4f3769ef0ff6
Park Hoijai <hoijae0194@naver.com>
2021-06-24 17:08:27 +0900

    Python Algorithm

---
+++ BOJ_Python/1406.py
@@ -0,0 +1,27 @@
+# 에디터
+import sys
+input = lambda :sys.stdin.readline().rstrip()
+
+left = list(input())
+right = []
+
+T = int(input())
+while T:
+    T -= 1
+    cmd = input()
+    if cmd[0] == 'L':
+        if len(left):
+            right.append(left.pop())
+    elif cmd[0] == 'D':
+        if len(right):
+            left.append(right.pop())
+    elif cmd[0] == 'B':
+        if len(left):
+            left.pop()
+    elif cmd[0] == 'P':
+        left.append(cmd[2])
+
+ans = left
+while len(right):
+    ans.append(right.pop())
+print(''.join(ans))
