a4569ab8462102f2a3b5251a3ea752d88fe13617
par3k <hoijae0194@naver.com>
2021-06-29 16:59:04 +0900

    Python Algorithm

--- BOJ_Python/1753_3.py
+++ BOJ_Python/1753_3.py
@@ -1,6 +1,5 @@
-# 최단경로 (다익스트라)
-import heapq, sys
-input = lambda :sys.stdin.readline().rstrip()
+# 특정한 최단 경로
+import sys, heapq
 
 v, e = map(int, input().split())
 k = int(input())
@@ -13,22 +12,22 @@ for _ in range(e):
     graph[from_].append((to_, weight))
 
 
-def dijikstra(start):
-    distance[start] = 0
+def dijikstra(node):
+    distance[node] = 0
     heap = []
-    heapq.heappush(heap, (distance[start], start))
+    heapq.heappush(heap, (distance[node], node))
     while heap:
-        c_distance, c_node = heapq.heappop(heap)
-        if distance[c_node] < c_distance: continue
-        for n_node, n_distance in graph[c_node]:
-            if c_distance + n_distance < distance[n_node]:
-                distance[n_node] = c_distance + n_distance
+        c_dist, c_node = heapq.heappop(heap)
+        if distance[c_node] < c_dist: continue
+        for n_node, n_weight in graph[c_node]:
+            if c_dist + n_weight < distance[n_node]:
+                distance[n_node] = c_dist + n_weight
                 heapq.heappush(heap, (distance[n_node], n_node))
 
 
 dijikstra(k)
-for i in range(1, len(distance)):
-    if distance[i] == int(1e9):
+for i in distance[1:]:
+    if i == int(1e9):
         print("INF")
     else:
-        print(distance[i])
+        print(i)
