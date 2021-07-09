a96fcc2a0d5791680a78ce14b8bd3bb48fa5c281
hoijae0194 <hoijae0194@naver.com>
2021-07-09 10:34:22 +0900

    Python Algorithm

--- Programmers/소수찾기.py
+++ Programmers/소수찾기.py
@@ -1,18 +1,39 @@
+from itertools import permutations
 import math
 
-n = int(input())
+numbers = '17'
 
-is_Prime = [True] * (n+1)
-max_length = math.ceil(math.sqrt(n))
-ans = []
 
-for i in range(2, max_length):
-    if is_Prime[i]:
-        for j in range(i + i, n+1, i):
-            is_Prime[j] = False
+def Eratosthenes(n): # 에라토스테네스의 체로 소수 리스트 제작
+    flag = [True for _ in range(n + 1)]
 
-for i in range(2, n+1):
-    if is_Prime[i]:
-        ans.append(i)
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if flag[i] == True:
+            j = 2
+            while i * j <= n:
+                flag[i * j] = False
+                j += 1
 
-print(len(ans))
+    return [i for i in range(2, n + 1) if flag[i]]
+
+
+def solution(numbers):
+    tmp = []
+    for i in range(1, len(numbers) + 1):
+        tmp += list(map(''.join, permutations(numbers,i)))
+
+    arr = []
+    for i in range(len(tmp)):
+        arr.append(int(tmp[i]))
+    arr = list(map(int, set(arr)))
+
+    answer = 0
+    prime_number = Eratosthenes(max(arr))
+    for i in range(len(arr)):
+        if arr[i] in prime_number:
+            answer += 1
+
+    return answer
+
+
+print(solution(numbers))
