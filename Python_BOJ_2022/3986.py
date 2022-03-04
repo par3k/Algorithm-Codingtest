ad591be30a2636a1bd91b50c38fac94c76d358c3
Hoijae Park (Alex) <hoijae0194@naver.com>
2022-03-04 23:40:32 +0900

    [22.03.04] 3986 - 스택

---
+++ Python_BOJ_2022/3986.py
@@ -0,0 +1,18 @@
+# 좋은 단어
+
+ans = 0
+for _ in range(int(input())):
+    arr = []
+    s = input()
+    for i in range(len(s)):
+        if len(arr) == 0:
+            arr.append(s[i])
+        else:
+            if arr[-1] == s[i]:
+                arr.pop()
+            else:
+                arr.append(s[i])
+
+    if len(arr) == 0:
+        ans += 1
+print(ans)
