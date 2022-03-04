d9c97cef94adbc85b7f3a097b7cf178e53b4798a
Hoijae Park (Alex) <hoijae0194@naver.com>
2022-03-04 23:41:46 +0900

    [22.03.05] 20001 - 스택

---
+++ Python_BOJ_2022/20001.py
@@ -0,0 +1,22 @@
+# 고무오리 디버깅
+
+import sys
+arr = []
+
+while True:
+    s = sys.stdin.readline().rstrip()
+    if s == "고무오리 디버깅 끝":
+        if len(arr) == 0:
+            print("고무오리야 사랑해")
+            break
+        else:
+            print("힝구")
+            break
+    elif s == "문제":
+        arr.append(1)
+    elif s == "고무오리":
+        if len(arr) == 0:
+            for _ in range(2):
+                arr.append(1)
+        else:
+            arr.pop()
