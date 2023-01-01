8470137c1f1dda1236f4084632abc7c9c9d0e9b5
Hoijae Park (Alex) <hoijae0194@naver.com>
2023-01-01 16:36:28 +0900

    [23.01.01/Python] 2차원 행렬 덧셈

---
+++ Python_BOJ_2023/2738.py
@@ -0,0 +1,21 @@
+# 행렬 덧셈
+
+n, m = map(int, input().split())
+
+map1 = list()
+for i in range(n):
+    map1.append(list(map(int, input().split())))
+
+map2 = list()
+for i in range(n):
+    map2.append(list(map(int, input().split())))
+
+ans = list()
+for i in range(n):
+    for j in range(m):
+        ans.append(map1[i][j] + map2[i][j])
+
+for i in range(1, len(ans) + 1):
+    print(ans[i - 1], end=" ")
+    if i % 3 == 0:
+        print()
