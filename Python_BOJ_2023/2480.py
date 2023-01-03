02b4807a52fd937eee99e42a47be9524b0072719
Hoijae Park (Alex) <hoijae0194@naver.com>
2023-01-03 22:27:27 +0900

    [23.01.03/Python] 주사위 세개

---
+++ Python_BOJ_2023/2480.py
@@ -0,0 +1,13 @@
+# 주사위 세개
+a, b, c = map(int, input().split())
+
+if a == b == c:
+    print(10000 + a * 1000)
+elif a == b:
+    print(1000 + a * 100)
+elif b == c:
+    print(1000 + b * 100)
+elif c == a:
+    print(1000 + c * 100)
+else:
+    print(max(a, b, c) * 100)
