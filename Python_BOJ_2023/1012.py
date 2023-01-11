5ce1d7a9c616d215bea38a808b379feab141cd88
Hoijae Park (Alex) <hoijae0194@naver.com>
2023-01-11 22:56:36 +0900

    [23.01.11/Python] 유기농 배추

---
+++ Python_BOJ_2023/1012.py
@@ -0,0 +1,27 @@
+# 유기농 배추
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+def dfs(x, y):
+    visited[x][y] = True
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < n and 0 <= ny < m:
+            if not visited[nx][ny] and graph[nx][ny] == 1:
+                dfs(nx, ny)
+
+for _ in range(int(input())):
+    m, n, k = map(int, input().split())
+    graph = [[0 for _ in range(m)] for _ in range(n)]
+    visited = [[False for _ in range(m)] for _ in range(n)]
+
+    for _ in range(k):
+        y, x = map(int, input().split())
+        graph[x][y] = 1
+    
+    cnt = 0
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1 and not visited[i][j]:
+                dfs(i, j)
+                cnt += 1
+    print(cnt)
