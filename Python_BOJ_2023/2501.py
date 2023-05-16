d77b4c364ebc321d784ee90f639ff11404409126
박회재 <hoijae0194@naver.com>
2023-05-16 22:28:15 +0900

    [23.05.16/Python] BruteForce

---
+++ Python_BOJ_2023/2501.py
@@ -0,0 +1,13 @@
+# 약수 구하기
+
+n, k = map(int, input().split())
+
+yaksu = list()
+for i in range(1, n + 1):
+    if n % i == 0:
+        yaksu.append(i)
+
+if k > len(yaksu) - 1:
+    print(0)
+else:
+    print(yaksu[k - 1])
