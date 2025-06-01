0b5f901bb20541faa7d536dd23a140beb0037e4b
Alex <hoijae0194@naver.com>
2025-06-01 21:49:45 +0900

    [25.06.01/JAVA] String

---
+++ JAVA_BOJ_2025/Solve_2743.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solve_2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        System.out.println(input.length());
+    }
+}
